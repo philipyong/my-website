eda6677ed64db268876404f560ba9d28605e60e5
philip <philipyong@live.com.my>
2021-06-18 14:47:34 +0800

    remove

---
+++ build/precache-manifest.67153ffb8e48d374633807651b3a7b95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0514d413044f033f5927ca19abb6abaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcae73443c0af77c4856",
+    "url": "/static/css/main.24c586f8.chunk.css"
+  },
+  {
+    "revision": "52b1eb9559da4a5a8898",
+    "url": "/static/js/2.7a5ed7a9.chunk.js"
+  },
+  {
+    "revision": "6397acf4c5cafcd591a770ce33fc3e2f",
+    "url": "/static/js/2.7a5ed7a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcae73443c0af77c4856",
+    "url": "/static/js/main.31c2e6bb.chunk.js"
+  },
+  {
+    "revision": "fa78d55522bcc07761c2",
+    "url": "/static/js/runtime-main.c0b9b457.js"
+  }
+]);
